d46c2e60c0e11d0d93c43aa030940c3e3d3893d4
zhpadvoiski <zh.padvoiski@gmail.com>
2021-12-12 23:53:44 -0600

    test and code alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 import { Row } from "react-bootstrap";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
     const [items, setItems] = useState([]);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         axios
@@ -12,10 +15,15 @@ export default function Options({ optionType }) {
             .then((response) => setItems(response.data))
             .catch((error) => {
                 //TODO: handle error response
+                setError(true);
             });
     }, [optionType]);
-    //TODO: replace 'null' with Topping option when available
-    const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+    if (error) {
+        return <AlertBanner />;
+    }
+
+    const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
     const optionItems = items.map((item) => (
         <ItemComponent
--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -12,3 +12,19 @@ test("display image for each scoop option from server", async () => {
     const altText = scoopImages.map((img) => img.alt);
     expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("display image for each topping option from server", async () => {
+    render(<Options optionType="toppings" />);
+
+    const toppingImages = await screen.findAllByRole("img", {
+        name: /topping$/i,
+    });
+    expect(toppingImages).toHaveLength(3);
+
+    const altText = toppingImages.map((img) => img.alt);
+    expect(altText).toEqual([
+        "Cherries topping",
+        "M&Ms topping",
+        "Hot fudge topping",
+    ]);
+});
