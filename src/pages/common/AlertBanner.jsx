5c80b540e3d02aa3de3c63c474065ef561f5300e
zhpadvoiski <zh.padvoiski@gmail.com>
2021-12-20 17:30:48 -0600

    changes

--- src/pages/common/AlertBanner.jsx
+++ src/pages/common/AlertBanner.jsx
@@ -1,7 +1,6 @@
 import { Alert } from "react-bootstrap";
 
 export default function AlertBanner({ message, variant }) {
-    const unnecceseryChange = `Hi, I'm useless!`;
     const alertMessage =
         message || "An unexpected error occured. Please try again later.";
     const alertVariant = variant || "danger";
